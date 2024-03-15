e2083f74bf88df6cc0b30bc4620e00922ee6103c
drinkingkazu <kterao@slac.stanford.edu>
2024-03-15 08:35:53 -0700

    adding weights download

---
+++ bin/download_2x2_ckpt.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+gdown https://drive.google.com/uc?id=1L85RPV2rrWoxzOmMLb7tm7t65s07DPTi -O 2x2_siren.ckpt
+gdown https://drive.google.com/uc?id=1ofEXBe4YT715UrP6b6OD7d1_WCj2hIBP -O 2x2_siren_module0_iteration-1000000-epoch-1000000.ckpt
+gdown https://drive.google.com/uc?id=1i-rxqtRw6e9Qy1NfZIRuuU7loiKqjpN4 -O 2x2_siren_module1_iteration-1000000-epoch-1000000.ckpt
